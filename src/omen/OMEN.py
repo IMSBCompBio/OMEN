e4080d7f4df1d59fd34c2bb69690baf21c4cd72b
krastega0 <krastegar0@gmail.com>
2025-06-26 14:11:47 +0200

    changing the package structure

--- src/omen/OMEN.py
+++ src/omen/OMEN.py
@@ -1,37 +1,5 @@
-from spaco_py.SpaCoObject import SPACO
-import numpy as np
-from config_loader import sample_features_path, neighbor_matrix_path
-
+from .SpaCoObject import SPACO
 
 class OMEN(SPACO):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-    def _eigen_cutoff(self):
-        """
-        Function to determine the cutoff for eigenvalues, for the spectral filtering
-        method that is used in SPACO.
-        """
-
-        # creating a range of values to be put into replicate function from SPACO
-        value_range = np.arange(10, 21, 1)
-
-        # index for finding the middle of the range
-        index = int(len(value_range) / 2)
-
-        # call the replicate function from SPACO
-        self.replicate(value_range[index])
-
-
-if __name__ == "__main__":
-    # Loading .npy file
-    sf = np.load(sample_features_path, allow_pickle=False)
-    neighbor = np.load(neighbor_matrix_path, allow_pickle=False)
-
-    # calling SPACO class
-    omen = OMEN(sample_features=sf, neighbormatrix=neighbor)
-    # checking the keys of the loaded files
-    # print(type(sf), type(neighbor))
-
-    # getting the spac projections
-    Denoised_pattern, meta_patterns = omen.spaco_projection()
